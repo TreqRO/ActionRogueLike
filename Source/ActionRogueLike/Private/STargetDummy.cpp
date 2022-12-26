588700c16521aeeaf1c1b586829e7a358ea39990
BogdanObreja1 <obrejabogdan10@gmail.com>
2022-12-26 20:55:06 +0100

    Materials Basics

---
+++ Source/ActionRogueLike/Private/STargetDummy.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "STargetDummy.h"
+
+#include "SAttributeComponent.h"
+#include "Components/StaticMeshComponent.h"
+
+// Sets default values
+ASTargetDummy::ASTargetDummy()
+{
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>("MeshComp");
+	RootComponent = MeshComp;
+
+	AttributeComp = CreateDefaultSubobject<USAttributeComponent>("AttributeComp");
+	//Trigger when health is changed (damage/healing)
+	AttributeComp->OnHealthChanged.AddDynamic(this, &ASTargetDummy::OnHealthChanged);
+}
+
+void ASTargetDummy::OnHealthChanged(AActor* InstigatorActor, USAttributeComponent* OwningComp, float NewHealth, float Delta)
+{
+	// If its negative it means we get damaged
+	if (Delta < 0.0f)
+	{
+		// The name set on BP
+		// We pass the game time which is synchronous with that time node in our material 
+		MeshComp->SetScalarParameterValueOnMaterials("TimeToHit", GetWorld()->TimeSeconds);
+	}
+
+}
+
+
+
